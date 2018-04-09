8345709417bf591cab8c37381c9b79e0abb77da7
YOUR_USERNAME <nico@Nicos-MacBook-Pro.local>
2018-04-10 00:09:02 +0200

    locs

--- ex1.py
+++ ex1.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 from numpy import genfromtxt
 my_data = genfromtxt('traffic_per_hour.csv', delimiter='\t')
 
-my_data[np.isnan(my_data)] = 0
+my_data[np.isnan(my_data)] = np.mean(my_data)
 
-plt.plot(my_data[:,1])
-plt.ax
+#print(my_data[:,0])
+plt.scatter(my_data[:,0],my_data[:,1])
+plt.xlabel('Time(s)')
+plt.ylabel('F(t)')
 plt.show()
 
-print(my_data[0][1])
+z = np.polyfit(my_data[:,0], my_data[:,1], 3)
+z
